ba8da05e9b300aeff76bbb460b9c091f0aa4b465
Joshua David <Joshua@Joshua-David.net>
2014-05-23 17:28:35 -0700

    Simple space-finding FSM constructed

--- main.js
+++ main.js
@@ -1,94 +1,123 @@
-var N = 0, E = 1, S = 2, W = 3;
-function Component(name) {
-    if(this === window) return new Component(name);
-    this.name = name;
-    // Whether there is an input from this direction [N,E,S,W], though the actual
-    // directions change with the orientation
-    this.inputs =  [1, 1, 1, 1];
-    // Whether there is an output to this direstion
-    this.outputs = [1, 1, 1, 1];
-    // Orientations 0 through 3 are NESW respectively
-    this.orientation = N;
+function Tape(str) {
+    if(this === window) return new Tape(str);
+    var symbols = str.split('');
+    var position = 0;
+    var NUL = String.fromCharCode(0);
+    this.getCurr = function() {
+        return symbols[position];
+    }
+    this.setCurr = function(val) {
+        symbols[position] = val;
+    }
+    this.next = function() {
+        position += 1;
+        if(position >= symbols.length) {
+            symbols.push(NUL);
+        }
+    }
+    this.prev = function() {
+        if(position > 0) {
+            position -= 1;
+        } else {
+            symbols.unshift(NUL);
+        }
+    }
     this.show = function() {
-        var s = "";
-        s += ' ';
-        s += (this.inputs [(this.orientation + N) % 4]) ? 'v' : ' ';
-        s += (this.outputs[(this.orientation + N) % 4]) ? '^' : ' ';
-        s += ' <br />';
-        s += (this.outputs[(this.orientation + W) % 4]) ? '<' : ' ';
-        s += '++';
-        s += (this.inputs [(this.orientation + E) % 4]) ? '<' : ' ';
-        s += '<br />';
-        s += (this.inputs [(this.orientation + W) % 4]) ? '>' : ' ';
-        s += '++';
-        s += (this.outputs[(this.orientation + E) % 4]) ? '>' : ' ';
-        s += '<br />';
-        s += ' ';
-        s += (this.outputs[(this.orientation + S) % 4]) ? 'v' : ' ';
-        s += (this.inputs [(this.orientation + S) % 4]) ? '^' : ' ';
-        s += ' ';
-        return '<pre>' + s + '</pre>';
-    }
-    this.onInput = function(tape) {
-        return tape;
+        function sp_rpt(n) {
+            var s = '';
+            for(var i = 0; i < n; i++) s += ' ';
+            return s;
+        }
+        return '\n' + symbols.join('') + '\n' + sp_rpt(position) + '^\n';
     }
     return this;
 }
 
-function Tape(data) {
-    this.symbols = tape.split("");
-    this.index = 0;
-    this.direction = S;
-    return this;
-}
-
-function Machine(components) {
-
-}
-
-function Arrow() {
-    if(this === window) return new Arrow();
-    // Inherits from Component
-    Component.call(this, 'arrow');
-    // Takes input from all directions and redirects output in a single
-    // direction.
-    this.inputs  = [1, 1, 1, 1];
-    this.outputs = [1, 0, 0, 0];
-    // Default to upwards-facing arrow
-    this.show = function() {
-        return ["&#x2191;", "&#x2192;", "&#x2193;", "&#x2190;"][this.orientation];
-    }
-    return this;
-}
-
-function Branch() {
-    if(this === window) return new Branch();
-    Component.call(this, 'branch');
-}
-
-function showGrid(grid) {
-    function crel(tag, cls) {
-        var el = document.createElement(tag);
-        el.className = cls;
-        return el;
-    }
-    var table = crel("table", "grid");
-    var tr = crel("tr", "");
-    table.appendChild(tr);
-    for(var x = 0; x < grid[0].length; x++) {
-
-    }
-    for(var y = 0, row; row = grid[y]; y++) {
-        var tr = crel("tr", "");
-        table.appendChild(tr);
-        for(var x = 0; x < row.length; x++) {
-            var cpt = row[x];
-            var td = crel("td", "");
-            tr.appendChild(td);
-            if(cpt) {
-                td.innerHTML = cpt.show();
+function Component() {
+    if(this === window) return new Component();
+    this.inputs  = [null];
+    this.outputs = [null];
+    this.outputTo = function(out_id) {
+        var out = this.outputs[out_id];
+        return function(tape) {
+            if(out && out.act) {
+                out.act(tape);
             }
         }
     }
-    return table;
+    this.act = function(tape) {
+        this.outputTo(0)(tape);
+    }
+    return this;
 }
+
+function Logger(description) {
+    if(this === window) return new Logger(description);
+    Component.call(this);
+    this.act = function(tape) {
+        console.log('>' + description, tape.show());
+        this.outputTo(0)(tape);
+    }
+}
+
+function Starter(tape) {
+    if(this === window) return new Starter(tape);
+    Component.call(this);
+    this.act = function() {
+        this.outputTo(0)(tape);
+    }
+}
+
+function TesterForSpace() {
+    if(this === window) return new TesterForSpace();
+    Component.call(this);
+    this.outputs = [null, null];
+    this.act = function(tape) {
+        if(tape.getCurr() == " ") this.outputTo(0)(tape);
+        else                      this.outputTo(1)(tape);
+    }
+}
+
+function Nexter() {
+    if(this === window) return new Nexter();
+    Component.call(this);
+    this.act = function(tape) {
+        tape.next();
+        this.outputTo(0)(tape);
+    }
+}
+function Prever() {
+    if(this === window) return new Prever();
+    Component.call(this);
+    this.act = function(tape) {
+        tape.prev();
+        this.outputTo(0)(tape);
+    }
+}
+
+function Joiner() {
+    if(this === window) return new Joiner();
+    Component.call(this);
+}
+
+function link(from, out_id, to, in_id) {
+    from.outputs[out_id] = to;
+    to.inputs[in_id] = from;
+}
+
+var tape = Tape("Hello world");
+var starter = Starter(tape);
+var jnr = Joiner();
+var spt = TesterForSpace();
+var logT = Logger("True");
+var logF = Logger("False");
+var log2 = Logger("next");
+var nexter = Nexter();
+link(starter, 0, jnr, 0);
+link(jnr, 0, spt, 0);
+link(spt, 0, logT, 0);
+link(spt, 1, logF, 0);
+link(logF, 0, nexter, 0);
+link(nexter, 0, log2, 0);
+link(log2, 0, jnr, 1);
+starter.act();
